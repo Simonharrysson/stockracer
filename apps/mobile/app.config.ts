1c3baefcecce8131f8312f73e73f2d5178c77ac7
Simon Harrysson <simon.harrysson@gmail.com>
2025-10-28 21:51:24 +0100

    chore: M0 scaffold (Expo + Yarn + NativeWind + Supabase client)

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,8 @@
+export default {
+  name: "StockRacer",
+  slug: "stock-racer",
+  extra: {
+    SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
